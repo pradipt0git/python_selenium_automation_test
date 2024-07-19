b48659780cd75ab2ef359a276982bb814e25aab0
Surya P Pal <suryapal@in.ibm.com>
2024-07-19 16:45:23 +0530

    added with allure

--- src/Main.py
+++ src/Main.py
@@ -1,20 +1,44 @@
 from core.logger import Logger
 from core.driver import WebDriverManager
-from test_cases import tc01_login
+from test_cases import test_login                       
+from configparser import ConfigParser
+import pytest
+import allure
+from allure_commons.types import Severity
 
 class Initiate:
     def __init__(self):
         self.driver_manager = WebDriverManager()
         self.driver = self.driver_manager.get_driver()
         self.logger = Logger()
-        
-       
+        # self.readConfig()
 
-    def run_automation(self):        
-        tc = tc01_login.tc01_login("https://www.saucedemo.com/", self.driver,self.logger)
-        tc.step1()
+    # def readConfig(self):
+    #     # Specify the path to your configuration file
+    #     config_file = "config/config.properties"
+
+    #     # Create a ConfigParser object
+    #     config = ConfigParser()
+
+    #     # Read the configuration file
+    #     config.read(config_file)
+
+    #     # Access the properties using section and key names
+    #     self.selenium_driver = config['selenium']['driver']
+    #     self.test_case_url = config['test_case']['url']  
+
+    @allure.step("Login to the application")  
+    def test_login_functionality(self): 
+        allure.step("Step 1: Open the application")       
+        tc = test_login.test_login("https://www.saucedemo.com/", self.driver,self.logger)       
+        assert tc.test_1()
 
 
 if __name__ =="__main__":
     main_obj = Initiate()
-    main_obj.run_automation()
+    main_obj.test_login_functionality()
+
+
+
+
+   
--- src/test_cases/test_login.py
+++ src/test_cases/test_login.py
@@ -5,20 +5,24 @@ from selenium import webdriver
 from core.logger import Logger
 from utility import utility
 from test_cases import locator
+import allure
 
-class tc01_login:
+class test_login:
     def __init__(self,url: str, driver : webdriver.Chrome, logger: Logger):
         self.url = url
         self.driver = driver
         self.logger = logger
         self.locator = locator.locator(driver)
 
-    def step1(self):
+    @allure.step("Login to the application")
+    @allure.description("Login Successfull...")
+    @allure.label("testtttt")
+    def test_1(self):
         try:
             self.logger.info(f"Opening URL: {self.url}")
             self.driver.get(self.url)
             username_element = self.locator.get_user_name()
-
+            
             # Set the desired value in the username field
             username = "your_username"
             utility.text_entry(username_element, username)
@@ -30,6 +34,6 @@ class tc01_login:
             self.logger.error(f"Error occurred: {e}")
             return None
 
-        finally:
+        #finally:
             # Consider adding logic to close the browser based on your workflow
-            pass  # Placeholder for browser closure (if needed)
+            #pass  # Placeholder for browser closure (if needed)
